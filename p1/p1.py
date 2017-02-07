e1f406c2c394175ace9afec1f481293af532bad7
Jason Stein <jason.am.stein@gmail.com>
2017-02-07 17:10:51 -0500

    copied in some sample code, added ipynb

--- p1/p1.py
+++ p1/p1.py
@@ -1,1 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 import rdkit.Chem
+
+df_train = pd.read_csv("train.csv")
+df_test = pd.read_csv("test.csv")
+
+# store gap values
+Y_train = df_train.gap.values
+# row where testing examples start
+test_idx = df_train.shape[0]
+# delete 'Id' column
+df_test = df_test.drop(['Id'], axis=1)
+# delete 'gap' column
+df_train = df_train.drop(['gap'], axis=1)
+
+# DataFrame with all train and test examples so we can more easily apply feature engineering on
+df_all = pd.concat((df_train, df_test), axis=0)
+df_all.head()
+
+# Drop the 'smiles' column
+df_all = df_all.drop(['smiles'], axis=1)
+vals = df_all.values
+
+# Retrieving train and test post feature-engineering 
+X_train = vals[:test_idx]
+X_test = vals[test_idx:]
+
+def write_to_file(filename, predictions):
+    with open(filename, "w") as f:
+        f.write("Id,Prediction\n")
+        for i,p in enumerate(predictions):
+            f.write(str(i+1) + "," + str(p) + "\n")
