16035bdce1725c11964145baa14e259f6666b79d
Maxwell Levenson <mlevenson@college.harvard.edu>
2017-03-04 13:40:52 -0500

    added testing file for lstms

---
+++ p2/test.py
@@ -0,0 +1,23 @@
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load the dataset but only keep the top n words, zero the rest
+top_words = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=top_words)
+
+print X_train
+
+# truncate and pad input sequences
+max_review_length = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
+X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+
+print X_train
+print y_train
