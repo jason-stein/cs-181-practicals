e16081ab143878ed5608dcf2c1db52bf2a1e60fe
Wendy Woodin <wendy.woodin@gmail.com>
2017-04-04 18:15:23 -0400

    made clustering a class, working on predict

--- p3/clustering.py
+++ p3/clustering.py
@@ -1,26 +1,63 @@
 import util
 import numpy as np
 import time
+import csv
 from sklearn.cluster import MiniBatchKMeans
 
 
-def kmeans(k=200):
-	training = util.create_big_ass_matrix('train.csv')
-	training = np.array(training)
-	start = time.time()
-	training_log = np.where(training==0, 0, np.log(training) + 1)
-	print time.time() - start
-	start = time.time()
-	kmeans = MiniBatchKMeans(n_clusters=1000, batch_size=1000)
-	kmeans.fit(training_log)
-	print time.time() - start
-	print kmeans.cluster_centers_.shape
-	testing = util.create_big_ass_matrix('test.csv')
-	testing = np.array(testing)
-	testing_log = np.where(testing==0,0, np.log(testing)+1)
-	results = kmeans.predict(testing_log)
-	np.savetxt('kmeansres.csv', results, delimiter=',')
-	np.savetxt('kmeansclusters.csv', kmeans.cluster_centers_, delimiter=',')
+class kMeans():
+	def __init__(self):
+		self.K = -1
+		self.trainfile = 'train.csv'
+		self.labels = []
+		self.clusters = []
 
-kmeans()
+	def train(self, k=1000, trainfile='train.csv', batch=2000):
+		self.K = k
+		training = util.create_big_ass_matrix(trainfile)
+		training = np.array(training)
+		start = time.time()
+		training_log = np.where(training==0, 0, np.log(training) + 1)
+		print time.time() - start
+		start = time.time()
+		kmeans = MiniBatchKMeans(n_clusters=self.K, batch_size=batch, compute_labels=True)
+		kmeans.fit(training_log)
+		print time.time() - start
+		print kmeans.cluster_centers_.shape
+		self.labels = kmeans.labels_
+		self.clusters = kmeans.cluster_centers_
+		print kmeans.labels_.shape
+		np.savetxt('kmeansclusters.csv', kmeans.cluster_centers_, delimiter=',')
+		np.savetxt('kmeanslabels.csv', kmeans.labels_, delimiter=',')
+
+	def predict(self, testfile='test.csv', outfile='kmeans_results.csv'):
+		users = util.dict_from_csv("profiles_dict.csv")
+		artists = util.dict_from_csv("artists_dict.csv")
+
+		res = open(outfile, 'wb')
+		writer = csv.writer(res)
+		writer.writerow(['Id','plays'])
+
+		testing = csv.reader(open(testfile, 'r'))
+		next(testing, None)
+		i = 1
+
+		for row in testing:
+			if not i % 1000:
+				print i
+			writer.writerow([i, self.clusters[int(self.labels[users[row[1]]])][artists[row[2]]]])
+
+
+	# Predict
+	# mean = 118 # This is just a default in case a user has no info
+	# testing = util.create_big_ass_matrix('test.csv')
+	# testing = np.array(testing)
+	# testing_log = np.where(testing==0,0, np.log(testing)+1)
+	# results = kmeans.predict(testing_log)
+	# np.savetxt('kmeansres.csv', results, delimiter=',')
+	# np.savetxt('kmeansclusters.csv', kmeans.cluster_centers_, delimiter=',')
+
+# kmeans = kMeans()
+# kmeans.train()
+# kmeans.predict()
 
