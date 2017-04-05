c3d2a84eda430b07e55db7e0461c1b06fff0d8b9
Wendy Woodin <wendy.woodin@gmail.com>
2017-04-05 16:02:28 -0400

    country support latlng added and csv so y'all don't need to make it again

--- p3/util.py
+++ p3/util.py
@@ -1,6 +1,8 @@
 import csv
 import time
 import numpy as np
+import requests
+import json
 
 def dict_from_csv(direc):
     dic = {}
@@ -37,5 +39,49 @@ def create_big_ass_matrix(filename):
 
 	print "Time to create big_ass_matrix: {}".format(time.time() - start)
 	return big_ass_matrix
+
+def create_profile_matrix(filename):
+	countries = set()
+	with open(filename, 'r') as f:
+		data = csv.reader(f, delimiter=',', quotechar='"')
+		for line in data:
+			countries.add(line[3])
+	print len(countries)
+	print countries
+
+def create_country_dict():
+	countries = {}
+	url_base = "https://maps.googleapis.com/maps/api/geocode/json?address={}"
+	failed = []
+	with open('profiles.csv', 'r') as f:
+		data = csv.reader(f, delimiter=',', quotechar='"')
+		next(data, None)
+		i = 0
+		for line in data:
+			if not i % 10000:
+				print i
+			i += 1
+			if line[3] in countries.keys() or line[3] in failed:
+				continue
+			try:
+				response = requests.get(url_base.format(line[3]))
+				resp = json.loads(response.content)
+				lat = resp['results'][0]['geometry']['location']['lat']
+				lng = resp['results'][0]['geometry']['location']['lng']
+				countries[line[3]] = [lat, lng]
+			except Exception as inst:
+				print inst
+				print line[3]
+				failed.append(line[3])
+
+	print countries
+	with open('countries_latlng.csv', 'wb') as g:
+		w = csv.writer(g)
+		w.writerows(countries.items())
+
+	print failed
+
 # util.create_dict_csv('artists')
 # util.create_dict_csv('profiles')
+#create_profile_matrix('profiles.csv')
+create_country_dict()
