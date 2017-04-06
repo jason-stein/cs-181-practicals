14d90bc671d65b0d69acd5963d8e91fe32278a06
Wendy Woodin <wendy.woodin@gmail.com>
2017-04-05 21:23:07 -0400

    by cluster user median

--- p3/clustering.py
+++ p3/clustering.py
@@ -11,6 +11,7 @@ class kMeans():
 		self.trainfile = 'train.csv'
 		self.labels = []
 		self.clusters = []
+		self.cluster_medians = []
 
 	def train(self, k=200, trainfile='train.csv', batch=10000):
 		self.K = k
@@ -29,6 +30,40 @@ class kMeans():
 		print kmeans.labels_.shape
 		np.savetxt('kmeansclusters.csv', kmeans.cluster_centers_, delimiter=',')
 		np.savetxt('kmeanslabels.csv', kmeans.labels_, delimiter=',')
+
+	def train_by_profile(self, k=200, trainfile='train.csv', batch=10000):
+		# Train the kmeans
+		self.K = k
+		profiles = util.create_profile_matrix()
+		start = time.time()
+		kmeans = MiniBatchKMeans(n_clusters=self.K, batch_size=batch, compute_labels=True)
+		kmeans.fit(profiles)
+		print time.time() - start
+		self.labels = kmeans.labels_
+		self.clusters = kmeans.cluster_centers_
+
+		# Get median number of plays per artist per cluster
+		training = util.create_big_ass_matrix(trainfile)
+		self.cluster_medians = [[] for i in range(self.K)]
+		for i in range(self.K):
+			print i
+			data = training[np.where(self.labels == i)]
+			data_masked = np.ma.masked_where(data==0, data)
+			try:
+				self.cluster_medians[i] = np.ma.median(data_masked, axis=0).filled(0)
+			except IndexError:
+				# If there are no points associated with a cluster, it won't be able to be
+				# median-ed, print them out just to check
+				print data_masked
+				print data
+		self.training = training
+
+		self.cluster_medians = np.array(self.cluster_medians)
+		np.savetxt('kmeansclusters_profiles.csv', kmeans.cluster_centers_, delimiter=',')
+		np.savetxt('kmeanslabels_profiles.csv', kmeans.labels_, delimiter=',')
+		with open('kmeans_profiles_clustermedians.txt', 'wb') as f:
+			w = csv.writer(f)
+			w.writerows(self.cluster_medians)
 
 	def predict(self, testfile='test.csv', outfile='kmeans_results.csv'):
 		users = util.dict_from_csv("profiles_dict.csv")
@@ -50,7 +85,30 @@ class kMeans():
 			writer.writerow([i, int(round(val))])
 			i += 1
 
+	def predict_profile(self, testfile='test.csv', outfile='kmeans_profiles_results.csv'):
+		users = util.dict_from_csv("profiles_dict.csv")
+		artists = util.dict_from_csv("artists_dict.csv")
+
+		res = open(outfile, 'wb')
+		writer = csv.writer(res)
+		writer.writerow(['Id','plays'])
+
+		testing = csv.reader(open(testfile, 'r'))
+		next(testing, None)
+		i = 1
+
+		for row in testing:
+			if not i % 1000:
+				print i
+			val = self.cluster_medians[int(self.labels[int(users[row[1]])])][int(artists[row[2]])]
+			if val == 0:
+				val = np.median([j for j in self.training[int(users[row[1]])] if j != 0])
+				if val == 0:
+					val = 118 * .94
+			writer.writerow([i, int(round(val))])
+			i += 1
+
 kmeans = kMeans()
-kmeans.train()
-kmeans.predict()
+kmeans.train_by_profile(k=2000)
+kmeans.predict_profile(outfile='kmeans_profiles_results_2.csv')
 
--- p3/util.py
+++ p3/util.py
@@ -12,6 +12,17 @@ def dict_from_csv(direc):
         dic[str(row[0])] = row[1]
     return dic
 
+def country_dict_from_csv(file, normalize=True):
+	# Latitude runs between -90 and 90, longitude runs between -180 and 180
+	dic = {}
+	sys_map = csv.reader(open(file, 'r'))
+	for row in sys_map:
+		ll = eval(row[1])
+		if normalize:
+			ll = [ll[0]/90., ll[1]/180.]
+		dic[str(row[0])] = ll
+	return dic
+
 def create_dict_csv(file):
 	keys = dict_from_csv(file + ".csv").keys()
 	filename = "{}_dict.csv".format(file)
@@ -40,14 +51,37 @@ def create_big_ass_matrix(filename):
 	print "Time to create big_ass_matrix: {}".format(time.time() - start)
 	return big_ass_matrix
 
-def create_profile_matrix(filename):
-	countries = set()
-	with open(filename, 'r') as f:
+def create_profile_matrix():
+	start = time.time()
+	countries = country_dict_from_csv('countries_latlng.csv')
+	profiles_dict = dict_from_csv('profiles_dict.csv')
+	
+	profiles_matrix = np.zeros((len(profiles_dict), 3+2+3)) #m/f/unknown + age/unknown + lat/lng/unknown
+	with open('profiles.csv', 'r') as f:
 		data = csv.reader(f, delimiter=',', quotechar='"')
+		next(data, None)
 		for line in data:
-			countries.add(line[3])
-	print len(countries)
-	print countries
+			i = int(profiles_dict[line[0]])
+			if line[1] == 'm':
+				profiles_matrix[i][0] = 1
+			elif line[1] == 'f':
+				profiles_matrix[i][1] = 1
+			else:
+				profiles_matrix[i][2] = 1
+			if line[2]:
+				profiles_matrix[i][3] = int(line[2])/80.
+			else:
+				profiles_matrix[i][4] = 1
+			if line[3]:
+				try:
+					np.put(profiles_matrix[i], [5,6], countries[line[3]])
+				except Exception as inst:
+					print inst
+					profiles_matrix[i][7] = 1
+			else:
+				profiles_matrix[i][7] = 1
+	print "Time to create profiles_matrix: {}".format(time.time() - start)
+	return profiles_matrix
 
 def create_country_dict():
 	countries = {}
@@ -81,7 +115,7 @@ def create_country_dict():
 
 	print failed
 
+
 # util.create_dict_csv('artists')
 # util.create_dict_csv('profiles')
-#create_profile_matrix('profiles.csv')
-create_country_dict()
+#create_profile_matrix()
