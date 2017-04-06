5f7590c3a14654bdaf4113325740f28e4fa4f90c
Wendy Woodin <wendy.woodin@gmail.com>
2017-04-06 19:30:34 -0400

    adding validation to tmp.py

--- p3/tmp.py
+++ p3/tmp.py
@@ -11,16 +11,19 @@ class kMeans():
 		self.trainfile = 'train.csv'
 		self.labels = []
 		self.clusters = []
+		self.testing = False
 
-	def train(self, k=2000, trainfile='train.csv', batch=10000):
+	def train(self, k=2000, trainfile='train.csv', batch=10000, testing=True):
 		self.K = k
-		training = util.create_big_ass_matrix(trainfile)
-		training = np.array(training)
-		drop_zeroes = np.where(training == 0, np.nan, training)
+		self.testing = testing
+		if self.testing:
+			self.test, self.train = util.create_test_train(trainfile, test_amt=.2)
+		else:
+			self.train = util.create_big_ass_matrix(trainfile)
+		drop_zeroes = np.where(self.train == 0, np.nan, self.train)
 		self.user_medians = [np.nanmedian(user) for user in drop_zeroes]
-		print self.user_medians
 		start = time.time()
-		training_log = np.where(training==0, 0, np.log(training) + 1)
+		training_log = np.where(self.train==0, 0, np.log(self.train) + 1)
 		print time.time() - start
 		start = time.time()
 		kmeans = MiniBatchKMeans(n_clusters=self.K, batch_size=batch, compute_labels=True)
@@ -30,7 +33,6 @@ class kMeans():
 		self.labels = kmeans.labels_
 		self.clusters = kmeans.cluster_centers_
 		self.overall_averages = [np.mean(cluster) for cluster in self.clusters]
-		print self.overall_averages
 		print kmeans.labels_.shape
 		np.savetxt('kmeansclusters.csv', kmeans.cluster_centers_, delimiter=',')
 		np.savetxt('kmeanslabels.csv', kmeans.labels_, delimiter=',')
@@ -43,9 +45,13 @@ class kMeans():
 		writer = csv.writer(res)
 		writer.writerow(['Id','plays'])
 
-		testing = csv.reader(open(testfile, 'r'))
-		next(testing, None)
+		if self.testing:
+			testing = self.test
+		else:
+			testing = csv.reader(open(testfile, 'r'))
+			next(testing, None)
 		print "Predicting"
+		error = 0
 		for row in testing:
 			# if not row[0] % 1000:
 			# 	print row[0]
@@ -56,9 +62,16 @@ class kMeans():
 			val = 0 if val == 0 else np.exp(val - 1)
 			val -= self.overall_averages[label]
 			val += self.user_medians[user]
-			writer.writerow([row[0], int(round(val))])
+			if self.testing:
+				error += abs(val- int(row[3]))
+			else:
+				writer.writerow([row[0], int(round(val))])
+		if self.testing:
+			error = float(error) / len(self.test)
+			print "Error: {}".format(error)
+			return error
 
 kmeans = kMeans()
-kmeans.train()
+kmeans.train(k=200)
 kmeans.predict()
 
